519a6f943d2e492a96ee9b6ba92db5aaaa536366
MahiruShN <minkuan1704@gmail.com>
2023-10-18 15:34:19 +0700

    full ui

--- src/App.js
+++ src/App.js
@@ -16,8 +16,10 @@ function App() {
                 <h1>Wordle</h1>
             </nav>
             <AppContext.Provider value={{board, setBoard}}>
-                <Board/>
-                <Keyboard/>
+                <div className="game">
+                    <Board/>
+                    <Keyboard/>
+                </div>
             </AppContext.Provider>
 
         </div>
