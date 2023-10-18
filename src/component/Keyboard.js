49ba10f5f0fd42bdb4d7c5aee17e318927cc1406
MahiruShN <minkuan1704@gmail.com>
2023-10-18 23:58:40 +0700

    full keyboard

--- src/component/Keyboard.js
+++ src/component/Keyboard.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {userEffect} from 'react'
 import Key from './Key';
 
 function Keyboard() {
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+  userEffect (() => {
+    document.addEventListener("keydown", handleKeyboard)
+    return () => {
+    document.removeEventListener("keydown", handleKeyboard)
+
+    }
+  }, [handleKeyboard])
 
   return (
     <div className='keyboard'>
@@ -23,7 +31,7 @@ function Keyboard() {
         {keys3.map((key) => {
           return <Key keyVal={key}/>
         })}
-        <Key keyVal={"ENTER"} bigKey/>
+        <Key keyVal={"DELETE"} bigKey/>
 
       </div>
     </div>
