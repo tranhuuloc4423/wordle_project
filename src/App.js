4e9c996d2e2f56ff20a26e63b83c5c8dfe84d3fe
MahiruShN <minkuan1704@gmail.com>
2023-10-19 17:07:24 +0700

    .

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ function App() {
     const [board, setBoard] = useState(boardDefault);
     const [currAttempt, setCurrAttempt] = useState({attempt :0 , letterPos: 0})
 
+    const correctWord = "RIGHT"
+
     const onSelectLetter = (keyVal) => {
         if (currAttempt.letterPos > 4 ) return;
         const  newBoard = [...board]
@@ -37,7 +39,18 @@ function App() {
             <nav>
                 <h1>Wordle</h1>
             </nav>
-            <AppContext.Provider value={{board, setBoard, currAttempt, setCurrAttempt, onSelectLetter, onDelete, onEnter}}>
+            <AppContext.Provider value={{
+                board,
+                setBoard,
+                currAttempt,
+                setCurrAttempt,
+                onSelectLetter,
+                onDelete,
+                onEnter,
+                correctWord
+            
+                
+            }}>
                 <div className="game">
                     <Board/>
                     <Keyboard/>
